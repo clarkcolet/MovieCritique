cdf4975114e7708f27241fde72592a0929761bf4
Claudio Coletta <claudiocoletta@eroam-172-7.wifi.wmin.ac.uk>
2017-05-08 10:55:22 +0100

    Initial Commit

---
+++ TheMovieCritiqueV2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TheMovieCritiqueV2
+//
+//  Created by Claudio Coletta on 08/05/2017.
+//  Copyright © 2017 CCNN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
